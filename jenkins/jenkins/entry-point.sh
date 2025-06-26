6a55378a10c22b48c0da2c2bc3874d253975cf75
a.krizhanovsky <a.krizhanovsky@lamoda.ru>
2025-06-26 12:36:46 +0300

    v2

--- jenkins/jenkins/entry-point.sh
+++ jenkins/jenkins/entry-point.sh
@@ -5,16 +5,19 @@ sleep 5
 ssh-keyscan agent.jenkins.net >> ~/.ssh/known_hosts
 
 cp ~/.ssh/id_rsa.pub /ssh_keys/jenkins.pub
-cp ~/.ssh/id_rsa /ssh_keys/jenkins
 
-CLIENT_CERTIFICATE=$(cat /certs/client/cert.pem) # cat /certs/client/cert.pem
-CLIENT_KEY=$(cat /certs/client/key.pem)          # cat /certs/client/key.pem
-SERVER_CERTIFICATE=$(cat /certs/client/ca.pem)   # cat /certs/client/ca.pem
-
-sed -i "s~{CLIENT_CERTIFICATE}~$(echo $CLIENT_CERTIFICATE)~g" /var/jenkins_home/jenkins.yaml
-sed -i "s~{CLIENT_KEY}~$(echo $CLIENT_KEY)~g" /var/jenkins_home/jenkins.yaml
-sed -i "s~{SERVER_CERTIFICATE}~$(echo $SERVER_CERTIFICATE)~g" /var/jenkins_home/jenkins.yaml
-
-cat /var/jenkins_home/jenkins.yaml
+# TODO normal inject certificates
+#CLIENT_CERTIFICATE=$(cat /certs/client/cert.pem) # cat /certs/client/cert.pem
+#CLIENT_KEY=$(cat /certs/client/key.pem)          # cat /certs/client/key.pem
+#SERVER_CERTIFICATE=$(cat /certs/client/ca.pem)   # cat /certs/client/ca.pem
+#SSH_PRIVATE_KEY=$(cat ~/.ssh/id_rsa)             # cat /ssh_keys/jenkins
+#
+#sed -i "s~{CLIENT_CERTIFICATE}~$(echo $CLIENT_CERTIFICATE)~g" /var/jenkins_home/jenkins.yaml
+#sed -i "s~{CLIENT_KEY}~$(echo $CLIENT_KEY)~g" /var/jenkins_home/jenkins.yaml
+#sed -i "s~{SERVER_CERTIFICATE}~$(echo $SERVER_CERTIFICATE)~g" /var/jenkins_home/jenkins.yaml
+#
+#sed -i "s~{SSH_PRIVATE_KEY}~$(echo $SSH_PRIVATE_KEY)~g" /var/jenkins_home/jenkins.yaml
+#
+#cat /var/jenkins_home/jenkins.yaml
 
 jenkins.sh
